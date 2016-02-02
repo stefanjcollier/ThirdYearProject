51e580161f0b887a22d894b1d24d841574b44272
Stefan Collier <sc22g13@soton.ac.uk>
2016-02-02 11:36:23 +0000

    Profit_1 - Outline ProfitMargin Enum

--- src/sjc/dissertation/retailer/state/profit/ProfitMargin.java
+++ src/sjc/dissertation/retailer/state/profit/ProfitMargin.java
@@ -1,5 +1,31 @@
 package sjc.dissertation.retailer.state.profit;
 
 public enum ProfitMargin {
+	VeryHighProfitMargin("<++P>","Very High Profit Margin",0.2),
+	HighProfitMargin("<+P>","High Profit Margin",0.1),
+	NoProfitMargin("<0P>","No Profit (Margin)",0),
+	NegativeProfitMargin("<-P>","Negative Profit Margin", -0.1);
 
+	private final String sym;
+	private final String desc;
+	private final double prof;
+
+	private ProfitMargin(final String symbol, final String description, final double margin){
+		this.sym = symbol;
+		this.desc = description;
+		this.prof = margin;
+	}
+
+	@Override
+	public String toString(){
+		return "N/A";
+	}
+
+	public boolean equals(final ProfitMargin other){
+		return false;
+	}
+
+	public double getProfitMargin(){
+		return -666;
+	}
 }
