53a3682f872f86196e77d6d73bc078403a0e4428
Stefan Collier <sc22g13@soton.ac.uk>
2016-02-02 11:40:45 +0000

    Profit_1 - Completed based methods
    Need to include ProfitMargin.changeValue()

--- src/sjc/dissertation/retailer/state/profit/ProfitMargin.java
+++ src/sjc/dissertation/retailer/state/profit/ProfitMargin.java
@@ -1,5 +1,6 @@
 package sjc.dissertation.retailer.state.profit;
 
+//TODO javadoc PM
 public enum ProfitMargin {
 	VeryHighProfitMargin("<++P>","Very High Profit Margin",0.2),
 	HighProfitMargin("<+P>","High Profit Margin",0.1),
@@ -18,14 +19,29 @@ public enum ProfitMargin {
 
 	@Override
 	public String toString(){
-		return "N/A";
+		return this.desc;
 	}
 
+	/**
+	 * Determines if two instances of {@link ProfitMargin} are the same.
+	 *
+	 * @param other -- Another profit margin instance
+	 * @return true if they are the same
+	 */
 	public boolean equals(final ProfitMargin other){
-		return false;
+		return (this.sym == other.sym)
+				&& (this.desc == other.desc)
+				&& (this.prof == other.prof);
 	}
 
+
+	/**
+	 * Get the percentage value of the profit margin level.
+	 * I.e. the percentage that you would add/take-away from a product.s
+	 *
+	 * @return a percentage denoting profit margin
+	 */
 	public double getProfitMargin(){
-		return -666;
+		return this.prof;
 	}
 }
