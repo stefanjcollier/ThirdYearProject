d44d8aa612398e3b57fb66ec746d60871145522c
Stefan Collier <sc22g13@soton.ac.uk>
2016-02-04 21:56:37 +0000

    Consumer_1 - Implemented random choice
    Yet to make the chance generator

--- src/sjc/dissertation/consumer/Consumer.java
+++ src/sjc/dissertation/consumer/Consumer.java
@@ -1,11 +1,14 @@
 package sjc.dissertation.consumer;
 
+import java.util.Random;
+
 import sjc.dissertation.retailer.Retailer;
 
 public class Consumer {
 	private final int id;
 	private final String socClass;
 	private final double budget;
+	private static final Random rng = new Random();
 
 	protected Consumer(final int unseenId, final String socialClass, final double budget){
 		this.id = unseenId;
@@ -25,10 +28,22 @@ public class Consumer {
 		return this.budget;
 	}
 
+	/**
+	 * Determines whether this consumer can purchase their shop from the given retailer.
+	 *
+	 * @param retailer -- The retailer whom's price we are assessing
+	 * @return true if the consumer can purchase their shop from the retailer.
+	 */
 	public boolean canAfford(final Retailer retailer){
-		return false;
+		return costOfShop(retailer) <= this.budget;
 	}
 
+	/**
+	 * Returns the cost for this consumer to purchase their weekly shop.
+	 *
+	 * @param retailer -- The retailer whom's price we are assessing
+	 * @return The cost for the consumer of shopping with the given retailer
+	 */
 	public double costOfShop(final Retailer retailer){
 		return -Double.MIN_VALUE;
 	}
@@ -37,8 +52,54 @@ public class Consumer {
 		return 0;
 	}
 
-	public Retailer chooseRetailer(final Retailer[] allRetailers){
-		return null;
+	/**
+	 * Choose a retailer based on it's substate: Quality and Profit Margin
+	 *
+	 * @param retailers -- All the options
+	 * @return the chosen retailer
+	 */
+	public Retailer chooseRetailer(final Retailer[] retailers){
+		//Find the chance for choosing each retailer
+		final double[] chances = new double[retailers.length];
+		for (int re = 0; re < retailers.length; re++){
+			chances[re] = chanceOf(retailers, retailers[re]);
+		}
+		//Make a weighted random choice of the retailers
+		return retailers[probabilisticlyChoose(chances)];
+	}
+
+	/**
+	 * Makes a choice based on the various probabilities.
+	 *
+	 * e.g. chances=[0.7, 0.2, 0.1]
+	 *  There is a high chance of choosing option 1 however
+	 *  any option could be chosen.
+	 *
+	 *  @param chances -- The chances that all sum to 1
+	 *  @return the weighted random choice.
+	 */
+	private int probabilisticlyChoose(final double[] chances){
+		//Assume Sum(chances) = 1 i.e. Pie chart
+		//Choose a random point in a circle (circumference = 1)
+		final double randomPoint = rng.nextDouble();
+
+		//The start of the slice
+		double lowThresh = 0;
+
+		//The end of the slice
+		double highThresh = 0;
+
+		//Cycle through all 'slices' and see if that point is in that slice
+		for(int i = 0; i < chances.length; i++){
+			highThresh += chances[i];
+			//If the point is in the slice
+			if(lowThresh <= randomPoint && randomPoint < highThresh){
+				//Return the index of that slice
+				return i;
+			}
+			lowThresh = highThresh;
+		}
+		return -1;
 	}
 
 }
