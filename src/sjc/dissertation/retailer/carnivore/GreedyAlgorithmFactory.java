a67700bc1958aae28e5441fb1dfdd8ba3d57eede
Stefan Collier <sc22g13@soton.ac.uk>
2016-03-20 15:26:34 +0000

    Notes

--- src/sjc/dissertation/retailer/carnivore/GreedyAlgorithmFactory.java
+++ src/sjc/dissertation/retailer/carnivore/GreedyAlgorithmFactory.java
@@ -4,7 +4,7 @@ import sjc.dissertation.model.logging.MasterLogger;
 import sjc.dissertation.model.logging.wrappers.WrappedActionPredictor;
 import sjc.dissertation.retailer.Algorithm;
 
-//TODO: Actually use this!
+//TODO (GreedyFac) Actually use this!
 public class GreedyAlgorithmFactory {
 
 	private final MasterLogger logger;
