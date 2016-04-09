dfdb064634eb75064f09c4f4b7fa704f3b6f681e
Stefan Collier <sc22g13@soton.ac.uk>
2016-04-09 17:29:14 +0100

    Location_1 - Removed the last of the branch

--- src/sjc/dissertation/model/logging/wrappers/WrappedRetailer.java
+++ src/sjc/dissertation/model/logging/wrappers/WrappedRetailer.java
@@ -1,9 +1,6 @@
 package sjc.dissertation.model.logging.wrappers;
 
-import java.util.List;
-
 import sjc.dissertation.model.logging.MasterLogger;
-import sjc.dissertation.retailer.Branch;
 import sjc.dissertation.retailer.Retailer;
 import sjc.dissertation.retailer.state.RetailerState;
 import sjc.dissertation.retailer.state.profit.ProfitMargin;
@@ -69,18 +66,5 @@ public class WrappedRetailer implements Retailer, Wrapper{
 	public ProfitMargin getProfiMargin() {
 		return this.me.getProfiMargin();
 	}
-
-	@Override
-	public List<Branch> getBranches() {
-		return this.me.getBranches();
-	}
-
-	@Override
-	public Branch makeBranch(final int x, final int y) {
-		final Branch newBranch = this.me.makeBranch(x, y);
-		this.logger.trace("New Branch::"+newBranch);
-		return newBranch;
-	}
-
 
 }
